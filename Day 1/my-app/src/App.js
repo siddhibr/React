dceb33cd4d3f20bb328f14f729dc6e413973050a
siddhibr <siddhiborchate7@gmail.com>
2024-03-26 01:52:54 +0530

    updates

--- Day 1/my-app/src/App.js
+++ Day 1/my-app/src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
     <div className="App">
-      welcome
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+        
+      </Routes>
     </div>
   );
 }
